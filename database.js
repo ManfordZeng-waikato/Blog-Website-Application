c9d7e5a0dbad64006ca0498ac9d1b6b08966c475
Manford Zeng(cz174)-waikato <zengchang389@gmail.com>
2024-02-01 23:50:30 +1300

    connect to database and env

---
+++ database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mariadb = require("mariadb");
+
+const USER_NAME = process.env.DB_USER;
+const USER_PASS = process.env.DB_PASS;
+
+const database = mariadb.createConnection({
+    host: process.env.DB_HOST,
+    database: USER_NAME,
+    user: USER_NAME,
+    password: USER_PASS
+});
+
+module.exports = database;
